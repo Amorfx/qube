d0ae50be8a739e5fcc2f0103d86cfa2ae6515464
Clément Décou <clement.decou@ornikar.com>
2023-06-22 22:34:35 +0200

    start definition for not shareable services

---
+++ src/DependencyInjection/Definition.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Amorfx\Qube\DependencyInjection;
+
+use Closure;
+
+/**
+ * A definition describe a Service
+ * - The service can be shareable or not (only one instance or not)
+ * - Have the factory method
+ */
+class Definition
+{
+    public function __construct(
+        private readonly Closure $factory,
+        private readonly bool $isShared = true
+    ) {
+    }
+
+    public function create(ContainerInterface $container): object
+    {
+        return ($this->factory)($container);
+    }
+
+    public function isShared(): bool
+    {
+        return $this->isShared;
+    }
+}
---
+++ tests/DependencyInjection/DefinitionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Amorfx\Qube\Tests\DependencyInjection;
+
+use Amorfx\Qube\DependencyInjection\Definition;
+use PHPUnit\Framework\TestCase;
+
+class DefinitionTest extends TestCase
+{
+    public function test_it_initiate_is_shared_correctly(): void
+    {
+        $definition = new Definition(function(){}, false);
+        self::assertFalse($definition->isShared());
+    }
+
+    public function test_it_is_shared_default_true(): void
+    {
+        $definition = new Definition(function(){});
+        self::assertTrue($definition->isShared());
+    }
+}
