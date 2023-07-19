9e27b721142dab56eac5962ccd8501c9ecc54b62
Amorfx <Amorfx@users.noreply.github.com>
2023-07-19 21:12:01 +0000

    Fix styling

--- src/DependencyInjection/Builder/Validators/ServiceConfigValidator.php
+++ src/DependencyInjection/Builder/Validators/ServiceConfigValidator.php
@@ -15,7 +15,7 @@ class ServiceConfigValidator
         $expectedKeys = ['factory', 'object', 'tags', 'shared'];
         $diff = array_diff($keys, $expectedKeys);
 
-        if (!empty($diff)) {
+        if (! empty($diff)) {
             throw new InvalidArgumentException('Parameters for services config keys ' . implode(', ', $diff) . ' are not allowed. Availables keys are ' . implode(', ', $expectedKeys));
         }
 
@@ -23,24 +23,25 @@ class ServiceConfigValidator
         $haveFactory = in_array('factory', $keys);
         $haveObject = in_array('object', $keys);
         $shouldHaveFactoryOrObject = $haveFactory || $haveObject;
-        if (!$shouldHaveFactoryOrObject) {
+        if (! $shouldHaveFactoryOrObject) {
             throw new InvalidArgumentException('A service configuration should have factory or object.');
         }
 
         $object = $this->getObjectForService($payload, $haveFactory, $haveObject);
     }
 
-    private function getObjectForService(array $payload, bool $haveFactory, bool $haveObject): object {
+    private function getObjectForService(array $payload, bool $haveFactory, bool $haveObject): object
+    {
         $object = null;
         if ($haveFactory) {
-            if (!$payload['factory'] instanceof Closure) {
+            if (! $payload['factory'] instanceof Closure) {
                 throw new InvalidArgumentException('A service factory must be a Closure.');
             }
             $object = $payload['factory'];
         }
 
         if ($haveObject) {
-            if (!is_object($payload['object']) || $payload['object'] instanceof Closure) {
+            if (! is_object($payload['object']) || $payload['object'] instanceof Closure) {
                 throw new InvalidArgumentException('A service object must be an instance of a class but not of Closure object.');
             }
 
